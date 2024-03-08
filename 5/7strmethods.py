fb725f0d5004be6d521ee174a9bab3491e701e3b
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-08 21:26:11 +0530

    string methods

---
+++ 5/7strmethods.py
@@ -0,0 +1,12 @@
+#strings
+name = "Nandani Tiwari"
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.find('N'))
+print(name.find('T'))
+print(name.find('p'))
+print(name.find('Tiwari'))
+print(name.replace("Nandani", "Gunnu"))
+print("Nandani" in name) #find string
+
