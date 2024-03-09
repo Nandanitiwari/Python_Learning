c411b3ebebb03e657d39c5096bc50d45bdd73e38
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-09 09:02:01 +0530

    list using for loop

---
+++ 8/18.py
@@ -0,0 +1,8 @@
+#list using for loop
+marks = [98, 78, 89, "maths", 34, 67, 59 , "gunnu", 12]
+for score in marks:
+    print(score)
+
+print(marks)
+print(marks[1:8])#print 1 to 7
+print(marks[1:8:3]) #print from 1 to every 3rd element until 7
