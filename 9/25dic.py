0ea89bcc91c80ddb9d9646b9a9e056da2513c47b
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-09 09:08:42 +0530

    dictonary

---
+++ 9/25dic.py
@@ -0,0 +1,10 @@
+#dictonary
+marks = {"english" : 67, "chemistry": 90, "physics" : 100}
+#information = {"ram":"balram"}
+print(marks["chemistry"])
+marks["hindi"] = 89
+print(marks["hindi"])
+print(marks)
+
+marks["physics"]= 99
+print(marks)
