2d6180da923303b1b7caf23103df00ae8285c661
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-12 20:38:19 +0530

    Docstring

---
+++ 13/45Docstring.py
@@ -0,0 +1,11 @@
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+#doctring is just written right below the function name or right after the function body
+
+# PEP8 --> its provides guidelines and best prectice to write python code
+
+# ZEN of python --> import this(poem)
