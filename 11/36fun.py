fd266ad52756d1d356ce0e19714ddc2a3c81f9aa
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-12 20:31:19 +0530

    function

---
+++ 11/36fun.py
@@ -0,0 +1,22 @@
+def calGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a>b):
+      print("a is greater")
+    else:
+      print("b is greater")
+       
+def isLesser(a, b):
+   pass 
+
+a = 9
+b = 8
+calGmean(a, b)
+isGreater(a, b)
+
+c = 8 
+d = 7
+calGmean(c, d)
+isGreater(c, d)
