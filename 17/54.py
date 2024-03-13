0e97978abfd23f1d78dcf4ad1d0986435a523db5
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-13 20:56:42 +0530

    Value error

---
+++ 17/54.py
@@ -0,0 +1,10 @@
+#value error
+try:
+    num = int(input("Enter an integer: "))
+    a = [6, 3]
+    print(a[num])
+except ValueError:
+    print("Number entered is not an integer. ")
+
+except IndexError:
+    print(" Index Error ")
