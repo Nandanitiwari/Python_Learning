0f7663798d71d043fda43184b93b457cbabca55d
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-13 20:46:06 +0530

    set

---
+++ 14/47set.py
@@ -0,0 +1,15 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"calra", 45, False, 4.5, 19}
+print(info)
+
+student = {} #It is an empty dictionary not set
+print(type(student))
+
+employe = set()  #It is an empty set
+print(type(employe))
+
+#accessing a set
+for value in info:
+    print(value , end="   ")
