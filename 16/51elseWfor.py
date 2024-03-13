a94973f2b64e20a9c2e0982f34deb20257a3159a
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-13 20:53:08 +0530

    for with else

---
+++ 16/51elseWfor.py
@@ -0,0 +1,28 @@
+for i in range(5):
+    print(i)
+else:
+    print("Sorry no i")
+
+
+for j in []:
+    print[j]
+else:
+    print("sorry no j")
+ 
+
+for k in range(6):
+    print(k)
+    if k == 4:
+        break  #it means loop ended successfully
+else:
+    print("Sorry no k")  #here else content will be not printed
+
+print("next loop")
+n = 0
+while n<7:
+    print(n)
+    n = n+1
+    if n == 4:
+     break
+else:
+   print("sorry no n")   #here else content will be not printed
