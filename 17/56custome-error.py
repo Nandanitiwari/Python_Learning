910ce80354615dc4092a8e28e23b5c93750d453a
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-13 20:59:49 +0530

    custome error

---
+++ 17/56custome-error.py
@@ -0,0 +1,5 @@
+#error rais so proggram stops and nothing unexpected happens
+a = int(input("Enter any value between 5 and 9:-"))
+
+if(a<5 or a>9):
+    raise ValueError("value should be between 5 and 9")
