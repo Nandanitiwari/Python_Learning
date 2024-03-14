0e13c102b0cbc6972c96c7b07d646a58ae358c71
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-14 10:51:02 +0530

    reccomended approach to import*

---
+++ 19/65.py
@@ -0,0 +1,13 @@
+ #It is not an reccomended approachl
+# from math import *
+import math as math_buildin_python
+
+# import math as m
+
+result = math_buildin_python.sqrt(9) * math_buildin_python.pi
+print(result)
+
+# from math import pi,sqrt as s
+
+# result = s(9) * pi 
+# print(result)
