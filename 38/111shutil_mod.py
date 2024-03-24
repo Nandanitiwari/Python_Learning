ce7c8cfa234d70431d4786d99f5b64819fe740a1
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-24 11:12:15 +0530

    shutil-module

---
+++ 38/111shutil_mod.py
@@ -0,0 +1,10 @@
+# Shutil is a Python module that provides a higher level interface for working with the file and directories. The name "shutil" is short for shell utility.
+# It provides a convenient and efficient way to automate tasks that are commonly performed on files and directories. In this repl, we'll take
+# a closer look at the shutil module and its various functions and how they can be used in Python
+
+import shutil
+import os
+# shutil.copy("38/demo.py", "38/demo2.py")
+# shutil.copytree("38/tute","38/mytute")
+# shutil.move("38/tute/file.file", "38/file.file")
+os.remove("38/file.file")
