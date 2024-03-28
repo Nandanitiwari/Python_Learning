0ccdb4176d7ac430ced18cce550d36ba3f9026dc
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-03-28 10:53:40 +0530

    gather

---
+++ 40/120Gather.py
@@ -0,0 +1,24 @@
+import time
+import asyncio
+
+async def function1():
+   await asyncio.sleep(1)
+   print("func 1") 
+   return "Harry"
+  
+async def function2():
+   await asyncio.sleep(1)
+   print("func 2")
+
+async def function3():
+   await asyncio.sleep(4)
+   print("func 3")
+
+async def main():
+    L = await asyncio.gather(
+           function1(),
+           function2(),
+           function3(),
+        )
+    print(L)
+asyncio.run(main())
