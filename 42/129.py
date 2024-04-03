e44a756fb8d443f681324e749dde598feedbe59a
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-04-03 20:55:46 +0530

    Number System

---
+++ 42/129.py
@@ -0,0 +1,12 @@
+#Number_system
+
+#decimal to binary
+number = 60
+print("binary representation is {}".format(bin(number)))
+
+#decimal to octal
+print("octal representation is {}".format(oct(number)))
+
+#decimal to hexadecimal
+print("hexadecimal representation is {}".format(hex(number)))
+
