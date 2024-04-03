02d27f81e6005c1638a4d4055ab9f460a7fc2048
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-04-03 20:59:49 +0530

    practice Questions on List-Tuple

---
+++ clg/exp4.py
@@ -0,0 +1,26 @@
+#a). Write a program to demostrate the list in python
+#creation of list
+# thisList = ["apple","banana","cherry"]
+# print(thisList)
+# print(len(thisList))
+# print(thisList[1])
+
+# thisList = ["apple","banana","cherry","orange","kiwi","melon"]
+# print(thisList[2:5])
+
+#b).write a program tuple in Python
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+thistuple = ("apple","banana","cherry")
+print(len(thistuple))
+thistuple = ("apple",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ('abc', 32,True, 40, "male")
+print(tuple1)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[0])
+
