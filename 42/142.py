d69a262672925e89ea3ed01b0d9ffd27745d07c7
Nandanitiwari <133760290+Nandanitiwari@users.noreply.github.com>
2024-04-03 20:57:58 +0530

    math module

---
+++ 42/142.py
@@ -0,0 +1,12 @@
+#math function in python
+# alise in python (math as m)
+import math
+
+x = math.sqrt(25)
+print(x)
+
+print(math.floor(2.9))
+print(math.ceil(2.9))
+print(math.pow(3,2))
+print(math.pi)
+print(math.e)
